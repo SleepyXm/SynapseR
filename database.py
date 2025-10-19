9460fb143f7a6fe1b10409173b8140987e72ba35
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2025-10-19 18:23:04 +0100

    Update database.py

--- database.py
+++ database.py
@@ -2,4 +2,4 @@ import databases
 import os
 
 DATABASE_URL = os.environ.get("DATABASE")
-database = databases.Database(DATABASE_URL, min_size=1, max_size=2)
+database = databases.Database(DATABASE_URL)
