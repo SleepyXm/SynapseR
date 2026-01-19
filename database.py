cea5a69e537b9fc11726ff53111f882e8ce8ba6a
Sleepy <158308669+SleepyXm@users.noreply.github.com>
2026-01-19 09:58:18 +0000

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 import databases
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.environ.get("DATABASE")
+load_dotenv()
+
+
+DATABASE_URL = os.getenv("DATABASE")
 database = databases.Database(DATABASE_URL, statement_cache_size=0)
